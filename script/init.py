6e3517330d84abbbeadfa6a8daa63836e7c0d6f3
greatforce <victorien.lemoine@gmail.com>
2016-02-24 01:57:54 -0800

    init

--- script/init.py
+++ script/init.py
@@ -3,9 +3,7 @@
 import os
 
 pwmsitch = os.listdir('/sys/devices/platform/ocp/48300000.epwmss/48300100.ecap/pwm/')
-pwmservo = os.listdir('/sys/devices/platform/ocp/48300000.epwmss/48300100.ecap/pwm/')
-
-print pwmsitch
+pwmservo = os.listdir('/sys/devices/platform/ocp/48302000.epwmss/48302200.ehrpwm/pwm/')
 
 fichier = open("pwmsitch.txt", "w")
 fichier.write(pwmsitch[0])
